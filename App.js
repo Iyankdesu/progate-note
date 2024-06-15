08406ff26cacb8a89ccf29c4c8627dc25d28cda1
iyankdesu <iyankdesu@gmail.com>
2024-06-15 22:19:35 +0800

    feat: add feature edit and delete

--- App.js
+++ App.js
@@ -1,3 +1,61 @@
+// import React, { useState } from "react";
+// import Home from "./src/screens/home";
+// import AddNote from "./src/screens/addNote";
+// import EditNote from "./src/screens/editNote";
+
+// const CurrentPageWidget = ({
+// 	currentPage,
+// 	noteList,
+// 	setCurrentPage,
+// 	addNote,
+// }) => {
+// 	switch (currentPage) {
+// 		case "home":
+// 			return <Home noteList={noteList} setCurrentPage={setCurrentPage} />;
+// 		case "add":
+// 			return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+// 		case "edit":
+// 			return <EditNote />;
+// 		default:
+// 			return <Home />;
+// 	}
+// };
+
+// const App = () => {
+// 	const [currentPage, setCurrentPage] = useState("home");
+
+// 	const [noteList, setNoteList] = useState([
+// 		{
+// 			id: 1,
+// 			title: "Note pertama",
+// 			desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+// 		},
+// 	]);
+
+// 	const addNote = (title, desc) => {
+// 		const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+// 		setNoteList([
+// 			...noteList,
+// 			{
+// 				id,
+// 				title: title,
+// 				desc: desc,
+// 			},
+// 		]);
+// 	};
+
+// 	return (
+// 		<CurrentPageWidget
+// 			currentPage={currentPage}
+// 			setCurrentPage={setCurrentPage}
+// 			noteList={noteList}
+// 			addNote={addNote}
+// 		/>
+// 	);
+// };
+
+// export default App;
+
 import React, { useState } from "react";
 import Home from "./src/screens/home";
 import AddNote from "./src/screens/addNote";
@@ -8,14 +66,31 @@ const CurrentPageWidget = ({
 	noteList,
 	setCurrentPage,
 	addNote,
+	editNote,
+	currentNote,
+	setCurrentNote,
+	deleteNote,
 }) => {
 	switch (currentPage) {
 		case "home":
-			return <Home noteList={noteList} setCurrentPage={setCurrentPage} />;
+			return (
+				<Home
+					noteList={noteList}
+					setCurrentPage={setCurrentPage}
+					setCurrentNote={setCurrentNote}
+					deleteNote={deleteNote}
+				/>
+			);
 		case "add":
 			return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
 		case "edit":
-			return <EditNote />;
+			return (
+				<EditNote
+					setCurrentPage={setCurrentPage}
+					editNote={editNote}
+					currentNote={currentNote}
+				/>
+			);
 		default:
 			return <Home />;
 	}
@@ -23,7 +98,7 @@ const CurrentPageWidget = ({
 
 const App = () => {
 	const [currentPage, setCurrentPage] = useState("home");
-
+	const [currentNote, setCurrentNote] = useState(null);
 	const [noteList, setNoteList] = useState([
 		{
 			id: 1,
@@ -38,10 +113,22 @@ const App = () => {
 			...noteList,
 			{
 				id,
-				title: title,
-				desc: desc,
+				title,
+				desc,
 			},
 		]);
+	};
+
+	const editNote = (id, title, desc) => {
+		const updatedNotes = noteList.map(note =>
+			note.id === id ? { ...note, title, desc } : note,
+		);
+		setNoteList(updatedNotes);
+	};
+
+	const deleteNote = id => {
+		const updatedNotes = noteList.filter(note => note.id !== id);
+		setNoteList(updatedNotes);
 	};
 
 	return (
@@ -50,6 +137,10 @@ const App = () => {
 			setCurrentPage={setCurrentPage}
 			noteList={noteList}
 			addNote={addNote}
+			editNote={editNote}
+			currentNote={currentNote}
+			setCurrentNote={setCurrentNote}
+			deleteNote={deleteNote}
 		/>
 	);
 };
